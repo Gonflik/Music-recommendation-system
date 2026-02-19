c83f7b31329655882dab25e2c4daff5c55d8ede5
danik322 <danylobucharov@gmail.com>
2026-02-19 18:51:44 +0200

    simple search_engine

---
+++ app/controller/search_engine.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, jsonify, request
+from app.model import Artist, Album, Song, Playlist
+
+search_bp = Blueprint('search', __name__)
+
+@search_bp.get('/search')
+def search():
+    query = request.args.get('q', default='', type=str).strip()
+
+    if not query:
+        return   jsonify({"message": "Query term is not provided!"}), 400
+
+    raw_result = {
+        "Artists": Artist.search_for_artist_by_query(query),
+        "Songs": Song.search_for_song_by_query(query),
+        "Albums": Album.search_for_album_by_query(query),
+        "Playlists": Playlist.search_for_playlist_by_query(query),
+    }
+
+    final_result = {}
+    for key, items in raw_result.items():
+        if items:
+            final_result[key] = [item.to_dict() for item in items]
+
+    return final_result
+
