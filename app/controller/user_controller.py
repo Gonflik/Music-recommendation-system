e24dd8368f31e953826973741f18eca01aa0f3fb
danik322 <danylobucharov@gmail.com>
2026-02-23 21:55:55 +0200

    +@jwt_required(not for all routes YET), /all for admin, also IBANI BUG if identity of JWT is user.id, VSO NAHUI, get invalid_token error

--- app/controller/user_controller.py
+++ app/controller/user_controller.py
@@ -1,14 +1,18 @@
 from flask import Blueprint, jsonify, request
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt
 from app.model import User
 from app.model.user import GenderEnum
 
 user_bp = Blueprint('user',__name__)
 
+
+#mozna zrobit decorator yakii bude chekat chi user ADMIN, abo prosto if else if else
+
 @user_bp.post('/register')
 def user_register():
     data = request.get_json()
     if not data.get('email'):
-        return jsonify({"message": "Email required!"}), 400
+        return jsonify({"error": "Email required!"}), 400
 
     user = User.get_user_by_email(data.get('email'))
     if user is not None:
@@ -27,7 +31,8 @@ def user_register():
         age = data.get('age'),
         password = hashed_pass,
         gender = GenderEnum(data.get('gender', 'prefer_not_to_say').lower()),
-        location = data.get('location')
+        location = data.get('location'),
+        role = data.get('role')
     )
     
     
@@ -40,18 +45,27 @@ def user_login():
     data = request.get_json()
     user = User.get_user_by_email(data.get('email'))
     if not user:
-        return jsonify({"message" : "User not found!"}), 404
+        return jsonify({"error" : "User not found!"}), 404
 
     password = data.get('password')
     if User.hash_password(password) == user.password:
-        return jsonify({"message": "Logged in!"}), 200
-    return jsonify({"message" : "Incorrect password!"}), 401
+        access_token = create_access_token(identity=user.email, additional_claims={"role": user.role}) 
+        refresh_token = create_refresh_token(identity=user.email, additional_claims={"role": user.role}) 
+        return jsonify({
+            "message": "Logged in!",
+            "tokens": {
+                "access": access_token,
+                "refresh": refresh_token
+            }
+        }), 200
+    return jsonify({"error" : "Incorrect password!"}), 401
 
 @user_bp.get('/profile/<int:id>')
+@jwt_required()
 def user_profile(id):
     user = User.get_user_by_id(id)
     if not user:
-        return jsonify({"message" : "User not found!"}), 404
+        return jsonify({"error" : "User not found!"}), 404
     return jsonify({"User" : {
         "email" : user.email,
         "id" : user.id,
@@ -61,12 +75,24 @@ def user_profile(id):
         "location": user.location
     }})
 
+@user_bp.get('/all')
+@jwt_required()
+def user_get_all():
+    claims = get_jwt()
+    if claims.get('role') == "admin":
+        page = request.args.get('page', default=1, type=int)
+        per_page = request.args.get('per_page', default=5, type=int)
+        all_users = User.get_all_users(page, per_page)
+        results = {"Users": [user.to_dict() for user in all_users]}
+        return jsonify(results), 200
+    return jsonify({"message": "You are not authorized to access this!"}), 401
+
 @user_bp.patch('/profile/<int:id>')
 def user_update_patch(id):
     data = request.get_json()
     user = User.get_user_by_id(id)
     if not user:
-        return jsonify({"message" : "User not found!"}), 404
+        return jsonify({"error" : "User not found!"}), 404
     
     new_name = data.get('name')
     new_age = data.get('age')
@@ -103,7 +129,7 @@ def user_update_put(id):
     data = request.get_json()
     user = User.get_user_by_id(id)
     if not user:
-        return jsonify({"message" : "User not found!"}), 404
+        return jsonify({"error" : "User not found!"}), 404
     
     if not all(key in data for key in ['name','age','gender','location']):
         return jsonify({"error" : "Missing requiered fields!"}), 400
@@ -131,14 +157,14 @@ def user_delete(id):
     data = request.get_json()
     user = User.get_user_by_id(id)
     if not user:
-        return jsonify({"message" : "User not found!"}), 404
+        return jsonify({"error" : "User not found!"}), 404
     
     if not data.get('password'):
-        return jsonify({"message": "Password required!"}), 400
+        return jsonify({"error": "Password required!"}), 400
 
     if user.check_password(data.get('password')):
         user.delete()
         return jsonify({"message" : "User deleted successfully!"}), 200
-    return jsonify({"message" : "Incorrect password!"}), 401
+    return jsonify({"error" : "Incorrect password!"}), 401
     
 
