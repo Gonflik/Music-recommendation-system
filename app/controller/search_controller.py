35dfd80e69ac5c785cd722d71fafe4dd0095e6d2
danik322 <danylobucharov@gmail.com>
2026-03-14 12:08:28 +0200

    changes for integration

--- app/controller/search_controller.py
+++ app/controller/search_controller.py
@@ -6,21 +6,22 @@ search_bp = Blueprint('search', __name__)
 @search_bp.get('/search')
 def search():
     query = request.args.get('q', default='', type=str).strip()
-
+    
     if not query:
         return   jsonify({"message": "Query term is not provided!"}), 400
 
+    artists = Artist.search_for_artist_by_query(query)
     raw_result = {
-        "Artists": Artist.search_for_artist_by_query(query),
-        "Songs": Song.search_for_song_by_query(query),
-        "Albums": Album.search_for_album_by_query(query),
-        "Playlists": Playlist.search_for_playlist_by_query(query),
+        "Artists": [artist.to_dict() for artist in artists],
+        #"Songs": Song.search_for_song_by_query(query),
+        #"Albums": Album.search_for_album_by_query(query),
+        #"Playlists": Playlist.search_for_playlist_by_query(query),
     }
     
-    final_result = {}
+    """final_result = {}
     for key, items in raw_result.items():
         if items:
-            final_result[key] = [item.to_dict() for item in items]
+            final_result[key] = [item.to_dict() for item in items]"""
 
-    return jsonify(final_result)
+    return jsonify(raw_result)
 
