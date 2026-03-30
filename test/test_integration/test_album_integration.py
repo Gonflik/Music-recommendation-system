0063829aeee23b34d076f48189e5b0a532a85a9d
danik322 <danylobucharov@gmail.com>
2026-03-30 10:11:19 +0300

    integrational tests

--- test/test_integration/test_album_integration.py
+++ test/test_integration/test_album_integration.py
@@ -1,36 +1,37 @@
 import pytest
 from sqlalchemy import select
-from app.model import Artist, Album, Song
+from app.model import Album, Artist, Song
 
 @pytest.mark.integration
-def test_api_song_success(client, db_session, auth_data, mock_deezer_api_response_song):
+def test_api_album_success(client, auth_data, db_session, mock_deezer_api_response_album):
     headers = auth_data['headers']
-    response = client.get('/search?q=Luv (sic) pt5', headers=headers)
+    response = client.get('/search?q=If', headers=headers)
     assert response.status_code == 200
 
     data = response.get_json()
-    assert data["Songs"][0]["name"] == "Luv (sic) pt5"
-    assert data["Songs"][0]["artist_name"] == "Nujabes"
-    assert data["Songs"][0]["album_name"] == "Luv (sic) hexalogy"
+    assert data["Albums"][0]["name"] == "If"
+    assert data["Albums"][0]["artist_name"] == "Mindless Self Indulgence"
+    assert data["Albums"][0]["artist_id"] == 1
 
     artist = db_session.execute(
-        select(Artist).where(Artist.dzid == 12317)
+        select(Artist).where(Artist.dzid == 6767)
     ).scalar_one_or_none()
 
     album = db_session.execute(
-        select(Album).where(Album.dzid == 6246234)
+        select(Album).where(Album.dzid == 30212)
     ).scalar_one_or_none()
 
-    assert artist is not None
-    assert artist.name == "Nujabes"
-
     assert album is not None
-    assert album.name == "Luv (sic) hexalogy"
+    assert album.name == "If"
+
+    assert artist is not None
+    assert artist.name == "Mindless Self Indulgence"
+
 
 @pytest.mark.integration
-def test_api_song_empty_response(client, db_session, auth_data, mock_deezer_api_response_song_empty):
+def test_api_album_empty_response(client, auth_data, db_session, mock_deezer_api_response_album_empty):
     headers = auth_data['headers']
-    response = client.get('/search?q=Something-non-existent', headers=headers)
+    response = client.get('/search?q=Bebrosiki', headers=headers)
     assert response.status_code == 200
 
     data = response.get_json()
--- test/test_integration/test_artist_integrational.py
+++ test/test_integration/test_artist_integrational.py
@@ -1,20 +1,4 @@
 import pytest
-
-# @pytest.mark.integration
-# def test_get_data_success(client, mock_api_response, auth_data):
-#     headers = auth_data['headers']
-#     response = client.get('/search?q=Bobrik', headers=headers)
-#     assert response.status_code == 200
-#     data = response.get_json()
-#     assert data['Artists'][0]['name'] == "Bobrik"
-#     assert data['Artists'][0]['aliases'] == ["bobr"]
-#     mock_api_response.assert_called_once_with(query='Bobrik', limit=3)
-
-# @pytest.mark.integration
-# def test_get_data_failure(client, mock_api_response_none, auth_data):
-#     headers = auth_data['headers']
-#     response = client.get('/search?q=Bobrik', headers=headers)
-#     assert response.status_code == 404
     
 @pytest.mark.integration
 def test_api_artist_success(client, mock_deezer_api_response_artist, auth_data):
@@ -27,7 +11,7 @@ def test_api_artist_success(client, mock_deezer_api_response_artist, auth_data):
     assert data["Artists"][0]["dzid"] == 27
 
 @pytest.mark.integration
-def test_api_artist_failure(client, mock_deezer_api_response_artist_empty, auth_data):
+def test_api_artist_empty_response(client, mock_deezer_api_response_artist_empty, auth_data):
     headers = auth_data['headers']
     response = client.get('/search?q=MASS OF THE FERMENTING DREGS', headers=headers)
     assert response.status_code == 200
