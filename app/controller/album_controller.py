0c18ee2f184452ea812a6ae313bdde8305426070
danik322 <danylobucharov@gmail.com>
2026-04-15 17:51:50 +0300

    +action creation on every request

--- app/controller/album_controller.py
+++ app/controller/album_controller.py
@@ -1,23 +1,28 @@
 from flask import Blueprint, jsonify, request, url_for
 from flask_jwt_extended import jwt_required, current_user, get_jwt_identity
-from app.model import Rating, User, Album, Song, Artist
+from app.model import Rating, User, Album, Song, Artist, Action
+from app.model.action import ActionName
 
 album_bp = Blueprint('artist', __name__)
 
 @album_bp.get('/albums')
+@jwt_required()
 def album_index():
     page = request.args.get('page', default=1, type=int)
     per_page = request.args.get('per_page', default=5, type=int)
 
-
     #Album.get
 
 @album_bp.get('/albums/<int:album_id>')
+@jwt_required()
 def album_show(album_id):
     album = Album.get_album_by_id(album_id, load_songs=True)
     if not album:
         return jsonify({"error": "Album not found!"}), 404
-    album_dict = album.to_dict()
+    
+    Action.create_or_increment(name=ActionName.ALBUM_SHOW ,user_id=current_user.id, reference_id=album_id, reference_name="album")
+
+    album_dict = album.to_dict()    
     album_dict["Songs"] = sorted([s.to_dict() for s in album.songs], key=lambda song: song.get("song_position"))
 
     return jsonify({"Album": album_dict})
