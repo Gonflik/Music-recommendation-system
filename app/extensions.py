2c19d66c4b26b35f108cc9232e02d81808f503eb
danik322 <danylobucharov@gmail.com>
2026-07-18 15:20:30 +0300

    +redis for caching

--- app/extensions.py
+++ app/extensions.py
@@ -1,5 +1,9 @@
 from flask_sqlalchemy_lite import SQLAlchemy
 from flask_jwt_extended import JWTManager
+import redis
+import os
+
 
 db = SQLAlchemy()
 jwt = JWTManager()
+redis_client = redis.from_url(os.environ.get('REDIS_URL', 'redis://localhost:6379/0'))
