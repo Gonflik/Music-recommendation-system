737d680ed41892c60d08180893cd4043a57e8903
danik322 <danylobucharov@gmail.com>
2026-07-24 17:07:14 +0300

    -comments, +user can only change/delete his own account

--- app/controller/user_controller.py
+++ app/controller/user_controller.py
@@ -135,32 +135,11 @@ def user_logout():
 
     return jsonify({"message": "Logged out successfully!"}), 200
 
-"""@user_bp.patch('/profile')
-@jwt_required()
-def user_update_patch():
-    user = current_user
-    if not current_user:
-        return jsonify({"error" : "User not found!"}), 404
-    
-    data = request.get_json()
-    success, errors = user.update(data)
-    if errors:
-        status_code = errors.pop('code', 400)
-        return jsonify(errors), status_code
-    
-    return jsonify({"message" : "User updated successfully!",
-                    "User" : user.to_dict(),
-                    }), 200"""
-
 @user_bp.patch('/api/users/<int:user_id>')
 @jwt_required()
 def user_update_put(user_id):
-    current_user = User.get_user_by_id(user_id)
-    if not current_user:
-        return jsonify({"error" : "User not found!"}), 404
-    
-    if get_jwt_identity() != current_user.email:
-        return jsonify({"message": "You are not authorized to access this!"}), 401
+    if user_id != current_user.id:
+        return jsonify({"message": "You are not authorized to access this!"}), 403
     
     data = request.get_json()
     success, errors = current_user.update(data)
@@ -172,25 +151,20 @@ def user_update_put(user_id):
                     "User" : current_user.to_dict(),
                     }), 200
     
-    #user model has more attributes, do i need to put all of them, rn its only the NOT NULL ones
     
 @user_bp.delete('/api/users/<int:user_id>')
 @jwt_required()
 def user_delete(user_id):
     data = request.get_json()
-    user = User.get_user_by_id(user_id)
-    if not user:
-        return jsonify({"error" : "User not found!"}), 404
-    
-    if get_jwt_identity() != current_user.email:
-        return jsonify({"message": "You are not authorized to access this!"}), 401
+    if user_id != current_user.id:
+        return jsonify({"message": "You are not authorized to access this!"}), 403
     
     required_fields = ['password']
     missing = [miss for miss in required_fields if miss not in data]
     if missing:
         return jsonify({"error": "Missing required fields!", "missing": missing, }), 400
     
-    success, response = user.delete(data)
+    success, response = current_user.delete(data)
 
     status_code = response.pop('code')
     return jsonify(response), status_code
